42dad8bcddbd4291571a57c4a2b51f9beb76081b
gsmann612 <115675414+gsmann612@users.noreply.github.com>
2025-01-09 05:41:03 -0500

    Update validate_service.sh

--- procore-products-master/scripts/validate_service.sh
+++ procore-products-master/scripts/validate_service.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-
 # Check the status of Apache
 if systemctl is-active --quiet httpd; then
     echo "Apache is running."
     exit 0
 else
-    echo "Apache is not running."
+    echo "Apache is not running. Checking for any errors..."
+    sudo systemctl status httpd
     exit 1
 fi
